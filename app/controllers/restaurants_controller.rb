6c40416aac4b1a06d6dd0b5d5c5daa93d8b3b771
Joan <joancc@gmail.com>
2014-07-11 12:14:50 -0400

    Step 1. Add models, controllers and views

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,52 @@
+class RestaurantsController < ApplicationController
+  before_action :set_params, only: [:index]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to(action: 'index')
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to(action: 'show')
+    else
+      render('form')
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_params
+    @restaurants = Restaurant.all
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :phone)
+  end
+end
