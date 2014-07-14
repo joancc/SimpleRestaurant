ebdf758b79e90b450ab7602a2e94157b93190817
Joan <joancc@gmail.com>
2014-07-14 15:49:10 -0400

    Delete unnecessary files and actions from Access. Create User model for authentication purposes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :restaurants
 
+  validates :password, presence: true, length: { minimum: 8 }
   validates_uniqueness_of :email 
-  validates_presence_of :email
+  validates_presence_of :name, :email
   validates_confirmation_of :password
+  validates :password_confirmation, presence: true, :on => :create
 end
