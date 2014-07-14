35d5633ac5070efe05786de50c3d974f8ef96476
Joan <joancc@gmail.com>
2014-07-14 18:48:33 -0400

    Correct problems with migrations

---
+++ db/migrate/20140714222239_add_image_to_restaurants.rb
@@ -0,0 +1,9 @@
+class AddImageToRestaurants < ActiveRecord::Migration
+  def up
+    remove_column :restaurants, :image, :string
+  end
+
+  def down
+    add_column :restaurants, :image
+  end
+end
