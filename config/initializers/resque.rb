3c745254b0a6e917f096db5c88f4b407125b93d7
Joan <joancc@gmail.com>
2014-07-18 18:33:46 -0400

    Modify resque.rb

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,8 @@
 # Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each { |file| require file }
-  # YAML.load(File.read('config/development.yml'))
+  
 
-config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
-Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
+# config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
+# Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
+
+YAML.load(File.read('config/development.yml'))
+Resque.redis = Redis.new(:host => 'host', :port => 'port')
